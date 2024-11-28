7e151900aa194e3f9fe1cb3acc1ded5a96bfb265
RAKk77 <yorpheareak168@gmail.com>
2024-11-28 14:42:30 +0700

    Card for Learing small project

---
+++ public/app.js
@@ -0,0 +1,143 @@
+let allProducds = [
+  {
+    id: 1,
+    name: "C++",
+    title: "Learn Programing with C++",
+    image: "./images/im1.png",
+    price: "20$",
+    rating: "1",
+  },
+  {
+    id: 2,
+    name: "Java",
+    title: "Learn Programing with Java",
+    image: "./images/im2.png",
+    price: "25$",
+    rating: "2",
+  },
+  {
+    id: 3,
+    name: "JavaScript",
+    title: "Learn Programing with JavaScript",
+    image: "./images/im3.png",
+    price: "30$",
+    rating: "3",
+  },
+  {
+    id: 4,
+    name: "TypeScript",
+    title: "Learn Programing with TypeScript",
+    image: "./images/im4.png",
+    price: "20$",
+    rating: "4",
+  },
+  {
+    id: 5,
+    name: "PHP",
+    title: "Learn Programing with PHP",
+    image: "./images/img5.png",
+    price: "25$",
+    rating: "5",
+  },
+  {
+    id: 6,
+    name: "React",
+    title: "Learn Programing with React",
+    image: "./images/icon-6.png",
+    price: "25$",
+    rating: "6",
+  },
+];
+
+const body = document.querySelector("body");
+const products = document.getElementById("products");
+const shoping = document.getElementById("shoping");
+let inforshoping = document.querySelector(".inforshoping");
+const cancel = document.getElementById("cancel");
+const productlist = document.querySelector("#productlist");
+
+let checklist = [];
+function oninto() {
+  allProducds.forEach((item, key) => {
+    let div = document.createElement("div");
+
+    div.classList.add("item");
+    div.innerHTML = `
+    <div
+        class="shadow-fav w-[90%] rounded-t-[2.5rem] rounded-b-[1rem] bg-slate-800 flex flex-col items-center my-4 m-auto overflow-hidden "
+      >
+        <div class=" bg-black/60 w-full flex justify-center py-2 "><img src="${item.image}" width="200px" class="transform motion-safe:hover:scale-110  transition-all"/></div>
+        <div class="w-full px-8 pb-4">
+          <h2 class="text-white text-[1.3rem] mt-2">
+            ${item.name}
+          </h2>
+          <p class="text-white text-[1rem] mt-2">
+            ${item.title}
+          </p>
+          <div class="w-20 text-center ">
+            <p class="my-3 text-black bg-slate-300 rounded-3xl">${item.price}</p>
+          </div>
+
+          
+          <div>
+            <button onclick="addCard(${key})"
+              class="mt-2 animate-bounce px-[25px] py-[10px] rounded-3xl text-white bg-sky-500 hover:bg-sky-700 "
+            >
+              Add to card
+            </button>
+          </div>
+        </div>
+      </div>
+   `;
+    products.appendChild(div);
+  });
+}
+oninto();
+
+shoping.addEventListener("click", function () {
+  let showing = inforshoping.classList.contains("hidden", "translate-y-full");
+  if (showing) {
+    // Show popup with slide-down animation
+    inforshoping.classList.remove("hidden");
+    inforshoping.classList.add("animate-slideDown");
+  }
+});
+
+cancel.addEventListener("click", function () {
+  let hide = inforshoping.classList.contains("hidden");
+
+  if (!hide) {
+    inforshoping.classList.add("hidden");
+  }
+});
+
+function addCard(id) {
+  // console.log(allProducds[id]);
+  if (checklist[id] == null) {
+    checklist[id] = allProducds[id];
+  }
+  reload();
+}
+
+function reload() {
+  productlist.innerHTML = "";
+  checklist.forEach((item, key) => {
+    let li = document.createElement("li");
+    li.classList.add(
+      "flex",
+      "items-center",
+      "gap-5",
+      "p-2",
+      "bg-slate-400",
+      "rounded-3xl",
+      "mb-2"
+    );
+
+    li.innerHTML = `
+    <img src="${item.image}"  width="40px"/>
+    <p>${item.name}</p>
+    <span>${item.price}</span>
+    `;
+    productlist.appendChild(li);
+  });
+}
